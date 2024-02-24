99f754d541b23983b814fc012087c19d82711ec0
Oleg Novoselskyy <tatteddev@gmail.com>
2024-02-23 20:21:00 -0500

    final

--- BandNameGenerator/BandNameGenerator/BandNameGenerator.py
+++ BandNameGenerator/BandNameGenerator/BandNameGenerator.py
@@ -1,3 +1,4 @@
-name = input("What is your name?\n")
-
-print ("Hello, "  + name)
+print("Welcome to the Band Name Generator.")
+cityName = input("What is the name of the city you grew up in?\n")
+petName = input("What is your pets name?\n")
+print("Your band name could be " + cityName + " " + petName)
